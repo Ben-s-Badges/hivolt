494661eda298d250d355a50aa13b21580e3e3375
Hugo Fiennes <hfiennes@gmail.com>
2024-08-07 11:36:55 -0700

    Acclerometer mode added (joystick UP to activate) - thanks Toby\!

--- src/plugins/meter.py
+++ src/plugins/meter.py
@@ -6,15 +6,48 @@ class plugin_visual:
 
     #@micropython.native
     def meterUpdate(self):
-        self.left += self.dl
-        if (self.left >= self.max): self.dl = -1
-        if (self.left <= 0): self.dl = 1
-        self.top += self.dt
-        if (self.top >= self.max): self.dt = -1
-        if (self.top <= 0): self.dt = 1
-        self.right += self.dr
-        if (self.right >= self.max): self.dr = -1
-        if (self.right <= 0): self.dr = 1
+        if self.mode == 0:
+            self.left += self.dl
+            if (self.left >= self.max): self.dl = -1
+            if (self.left <= 0): self.dl = 1
+            self.top += self.dt
+            if (self.top >= self.max): self.dt = -1
+            if (self.top <= 0): self.dt = 1
+            self.right += self.dr
+            if (self.right >= self.max): self.dr = -1
+            if (self.right <= 0): self.dr = 1
+        if self.mode == 1:
+            x = 0.0
+            y = 0.0
+            z = 0.0
+            for i in range (0, len(self.xyz)):
+                x = x + self.xyz[i][0]
+                y = y + self.xyz[i][1]
+                z = z + self.xyz[i][2]
+            x = x / len(self.xyz)
+            y = y / len(self.xyz)
+            z = z / len(self.xyz)
+            self.left = int(x * 40.0)
+            if self.left < 0:
+                self.left = self.left + 80
+            if self.left < 0:
+                self.left = 0
+            if self.left > 80:
+                self.left = 80
+            self.top = int(y * 40.0)
+            if self.top < 0:
+                self.top = self.top + 80
+            if self.top < 0:
+                self.top = 0
+            if self.top > 80:
+                self.top = 80
+            self.right = int(z * 40.0)
+            if self.right < 0:
+                self.right = self.right + 80
+            if self.right < 0:
+                self.right = 0
+            if self.right > 80:
+                self.right = 80
 
     #@micropython.native
     def meterDraw(self):
@@ -27,7 +60,10 @@ class plugin_visual:
             sl = self.left - 40
             fl = 40
         for i in range(sl, fl):
-            self._screen[36-i] = 0xff
+            m = 36-i
+            if m < 0:
+                m = 0
+            self._screen[m] = 0xff
             self._screen[36+i] = 0xff
         if self.top <= 40:
             st = 0
@@ -45,8 +81,11 @@ class plugin_visual:
             sr = self.right - 40
             fr = 40
         for i in range(sr, ft):
+            p = 196+i
+            if p > 239:
+                p = 239
             self._screen[196-i] = 0xff
-            self._screen[196+i] = 0xff
+            self._screen[p] = 0xff
 
     def __init__(self, screen):
         self._screen = screen
@@ -57,6 +96,17 @@ class plugin_visual:
         self.dt = 1
         self.right = 0
         self.dr = 1
+        self.mode = 0
+        self.xyz = [(0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0),
+                    (0.0, 0.0, 0.0)]
         
     def draw(self):
         self.meterDraw()
@@ -64,11 +114,28 @@ class plugin_visual:
         return True
     
     def input(self, i):
-        if i[0] != plugin.EVENT_JOYSTICK:
-            return
-        if i[1] == plugin.JOYSTICK_LEFT:
-            if self._drawinterval > 1:
-                self._drawinterval -= 1
+        if i[0] == plugin.EVENT_JOYSTICK:
+            if i[1] == plugin.JOYSTICK_LEFT:
+                if self._drawinterval > 1:
+                    self._drawinterval -= 1
 
-        elif i[1] == plugin.JOYSTICK_RIGHT:
-            self._drawinterval += 1
+            elif i[1] == plugin.JOYSTICK_RIGHT:
+                self._drawinterval += 1
+
+            elif i[1] == plugin.JOYSTICK_UP:
+                if self.mode == 0:
+                    print("Switching to accelerometer mode")
+                    self.mode = 1
+                else:
+                    print("Switching to normal mode")
+                    self.mode = 0
+
+        if i[0] == plugin.EVENT_ACCEL:
+            xyz = i[1]
+            x = xyz[0] % 1
+            y = xyz[1] % 1
+            z = xyz[2] % 1
+            self.xyz.pop(0)
+            self.xyz.insert(len(self.xyz)-1, (x, y, z))
+            #print("Accel %d, %d, %d", xyz[0], xyz[1], xyz[2])
+
